cdc6e99c3421795b55d670fe7c7b1baf9c113a10
Eric <51809267+Zhiwei-Lu@users.noreply.github.com>
2021-04-29 11:08:15 +0200

    Add files via upload

---
+++ test scripts/run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+mkdir total_plot
+cp plot.plt total_plot
+cd total_plot
+sed -i '' '/moment_tot.dat/d' ./plot.plt
+sed -i  '' "s/Spin_dynamics/Spin dynamics with different non-local damping/g "  plot.plt
+cd ..
+for ii in {1..7}
+do
+mkdir damp_type_$ii
+cp damping_test.f90 plot.plt damp_type_$ii
+cd damp_type_$ii
+
+sed -i '' "s/d_type=1/d_type=$ii/g" damping_test.f90 
+gfortran damping_test.f90 
+./a.out
+if [ $ii == 1 ]
+then 
+sed -i  '' "s/Spin_dynamics/Diagonal onsite damping/g "  plot.plt
+cp moment_tot.dat ../total_plot/Diagonal-onsite-damping.dat
+# echo "plot \"../damp_type_$ii/moment_tot.dat\" using 1:6 title \"Diagonal onsite damping\" with linespoints" >> ../total_plot/plot.plt
+elif [ $ii == 2 ]
+then 
+sed -i  '' "s/Spin_dynamics/Different onsite damping/g "  plot.plt
+cp moment_tot.dat ../total_plot/Different-onsite-damping.dat
+elif [ $ii == 3 ]
+then 
+sed -i  '' "s/Spin_dynamics/Full onsite damping/g  "  plot.plt
+cp moment_tot.dat ../total_plot/Full-onsite-damping.dat
+
+elif [ $ii == 4 ]
+then 
+sed -i  '' "s/Spin_dynamics/Diagonal nonlocal damping/g  "  plot.plt
+cp moment_tot.dat ../total_plot/Diagonal-nonlocal-damping.dat
+
+elif [ $ii == 5 ]
+then 
+sed -i '' "s/Spin_dynamics/Full nonlocal damping/g "  plot.plt
+cp moment_tot.dat ../total_plot/Full-nonlocal-damping.dat
+
+elif [ $ii == 6 ]
+then 
+sed -i '' "s/Spin_dynamics/Negative full nonlocal damping/g "  plot.plt
+cp moment_tot.dat ../total_plot/Negative-full-nonlocal-damping.dat
+
+elif [ $ii == 7 ]
+then 
+sed -i '' "s/Spin_dynamics/Negative diagonal nonlocal damping/g "  plot.plt
+cp moment_tot.dat ../total_plot/Negative-diagonal-nonlocal-damping.dat
+
+fi
+
+
+
+gnuplot plot.plt
+printf $ii \n 
+cd ..
+done
+cd total_plot
+echo "filenames = \"Diagonal-onsite-damping Different-onsite-damping Full-onsite-damping Diagonal-nonlocal-damping Full-nonlocal-damping Negative-full-nonlocal-damping Negative-diagonal-nonlocal-damping\""  >> plot.plt
+echo "plot for [file in filenames] file.\".dat\" using 1:6 with lines lw 5" title "file"  >> plot.plt
+gnuplot plot.plt
+cd ..
